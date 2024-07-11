bde6c7f4ce85dc9fbc1edb4b6ab967b86e7b5a7a
ermonindima <ermonindima0@gmail.com>
2024-07-11 07:13:33 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -33,34 +33,54 @@ formText.addEventListener('focus', ()=>{
 const addNoteForm = document.querySelector('#addNoteForm');
 const noteTextInput = document.querySelector('#noteText');
 const cardswrapper = document.querySelector('#cards-wrapper');
-
 const noteTextInputTwo = document.querySelector('#noteTextInputTwo');
 
 
-const notes = [
-  {
-  title: 'Основы',
-  text: 'Lorem ipsum dolor sit amet'
-},
-{
-  title: 'Основы',
-  text: 'Lorem ipsum dolor sit amet'
-},
-{
-  title: 'Основы',
-  text: 'Lorem ipsum dolor sit amet'
-},
+let notes = [];
 
-  
-];
+if(localStorage.getItem('notes')){
+  notes = JSON.parse(localStorage.getItem('notes'));
+
+}else{
+
+}
+
+
+
+// const notes = [
+//   {
+//     id: 1,
+//   title: 'Основы',
+//   text: 'Lorem ipsum dolor sit amet'
+// },
+// {
+//   id: 2,
+//   title: 'Основы',
+//   text: 'Lorem ipsum dolor sit amet'
+// },
+// {
+//   id: 3,
+//   title: 'Основы',
+//   text: 'Lorem ipsum dolor sit amet'
+// },];
+
+
 
 
 addNoteForm.addEventListener('submit', (event)=>{
  event.preventDefault();
+
+ let id = 1;
+if( notes.length > 0 ){
+  id = notes[notes.length -1]['id'] + 1;
+}
  notes.push({
+  id: id,
   title: noteTextInput.value,
-  text: noteTextInputTwo.textContent,
+  text: noteTextInputTwo.innerHTML,
  });
+
+ localStorage.setItem('notes', JSON.stringify(notes));
  noteTextInputTwo.textContent = '';
  addNoteForm.reset();
  const lastNote = notes[notes.length - 1];
@@ -68,18 +88,25 @@ addNoteForm.addEventListener('submit', (event)=>{
         <div class="card-body">
             <h5 class="card-title">${lastNote.title}</h5>
           <p class="card-text">${lastNote.text}</p>
+          <button data-id="${lastNote.id}" data-action="delete" class="btn  btn-danger btn-sm">Удалить</button>
         </div>
       </section>`;
 cardswrapper.insertAdjacentHTML('afterbegin',html);
 noteTextInput.focus();
 });
-notes.forEach((item)=>{
-  console.log(item);
 
+
+
+
+
+
+notes.forEach((item, index)=>{
+  console.log(item);
   const html = `<section class="card">
         <div class="card-body">
             <h5 class="card-title">${item.title}</h5>
           <p class="card-text">${item.text}</p>
+          <button data-id="${item.id}" data-action="delete" class="btn btn-danger btn-sm">Удалить</button>
         </div>
       </section>`;
       cardswrapper.insertAdjacentHTML('afterbegin',html);
@@ -92,3 +119,20 @@ document.addEventListener('click', (event)=>{
 })
 
 
+
+
+// delete button
+
+document.addEventListener('click', (event)=>{
+  event.target
+  if(event.target.dataset.action === 'delete'){
+
+    const id = event.target.dataset.id;
+    const index = notes.findIndex((item) => {
+      return item.id == id;
+    })
+    notes.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+     event.target.closest('.card').remove()
+  }
+})
